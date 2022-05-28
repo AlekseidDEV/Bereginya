ec49feea1c556ddc63097659185ec4ed262d5ae4
lexa5461 <84816879+lexa5461@users.noreply.github.com>
2022-05-28 12:48:19 +0300

    test php

---
+++ index.php
@@ -0,0 +1,26 @@
+<header class="header">
+      <div class="container">
+        <div class="block__header">
+          <div class="block__adress">
+            <p class="adress">
+              Адрес завода: 140127, Московская область, Раменский р-н, пос.
+              санатория «Раменское»
+            </p>
+          </div>
+          <div class="block__contact">
+            <div class="span__block">
+              <img src="./img/icon/whatsapp1.svg" alt="img" class="what" />
+              <p class="numb">8 (903) 578-85-77</p>
+            </div>
+            <div class="span__block_teleg">
+              <img src="./img/icon/telegram2.svg" alt="img" class="what" />
+              <p class="adress__teleg">@bereginya.ru</p>
+            </div>
+            <div class="span__block_not">
+              <img src="./img/icon/interface.svg" alt="img" class="what" />
+              <a class="adress__email">info@bereginya.ru — написать письмо</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
