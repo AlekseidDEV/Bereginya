49b509049ca75b6d91134cba7d0f59874a0f7f72
lexa5461 <84816879+lexa5461@users.noreply.github.com>
2022-06-01 16:05:01 +0300

    update

--- js/catalogModal.js
+++ js/catalogModal.js
@@ -19,3 +19,46 @@ modalBasket.addEventListener("click", (e) => {
     modalBasket.style.display = "none";
   }
 });
+
+
+//two wind
+const btnCall = document.querySelector(".btn_panel_bran_open");
+const modalTwo = document.querySelector(".bg__modal_two");
+const closeModalTwo = document.querySelector(".close_thank");
+
+btnCall.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalTwo.style.display = "flex";
+});
+
+closeModalTwo.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalTwo.style.display = "none";
+});
+
+modalTwo.addEventListener("click", (e) => {
+  if (e.target == modalTwo) {
+    modalTwo.style.display = "none";
+  }
+});
+
+//free wind
+
+const btnIssue = document.querySelector(".issue");
+const modalFreeWind = document.querySelector(".modal__free_bg");
+
+btnIssue.addEventListener("click", (e) => {
+  e.preventDefault();
+  modalFreeWind.style.display = "flex";
+});
+
+
+modalFreeWind.addEventListener("click", (e) => {
+  if (e.target == modalFreeWind) {
+    modalFreeWind.style.display = "none";
+  }
+});
+
+
+
+
